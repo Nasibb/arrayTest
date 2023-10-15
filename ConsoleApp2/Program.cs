945faa519343badcd1ad6f8ddcb6ac50393e864b
Nasibb <nasibgi@code.edu.az>
2023-10-15 18:21:27 +0400

    first ready

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,20 @@
+﻿//int[] numbers = { 1,2,3,10,85,89 };
+//int z = 1;
+//foreach (var number in numbers)
+//{
+//    if (number >1 && number<=20)
+//    z *= number;
+//}
+//Console.WriteLine(z);
+
+
+int[] numbers = { 1, 5, 6, 8, 9, 11 };
+int sum = 0;
+foreach (var number in numbers)
+{
+    if (number % 2 == 0)
+    {
+        sum += number;
+    }
+}  
+Console.WriteLine(sum*sum);
